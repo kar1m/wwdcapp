0d1dddecba841494f91aa12947d1c63a2c200513
Karim <karim2904@gmail.com>
2015-04-19 23:34:56 +0100

    Intro animation

---
+++ KarimBenhmida/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  KarimBenhmida
+//
+//  Created by Karim Benhmida on 19/04/2015.
+//  Copyright (c) 2015 Karim Benhmida. All rights reserved.
+//
+
+#ifndef KarimBenhmida_BridgingHeader_h
+#define KarimBenhmida_BridgingHeader_h
+
+#import <pop/POP.h>
+
+#endif
--- KarimBenhmida/ViewController.swift
+++ KarimBenhmida/ViewController.swift
@@ -18,7 +18,32 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        
+        var test = NSEC_PER_SEC
+        
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
+            
+            var scaleAnimation = POPBasicAnimation(propertyNamed: kPOPViewScaleXY);
+            scaleAnimation.toValue = NSValue(CGPoint: CGPoint(x: 0.5, y: 0.5))
+            scaleAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+            scaleAnimation.duration = 0.5
+            self.appLogo.pop_addAnimation(scaleAnimation, forKey: "scaleAnimation")
+            
+            var frameAnimation = POPBasicAnimation(propertyNamed: kPOPLayerPosition)
+            frameAnimation.toValue = NSValue(CGPoint: CGPoint(x: self.appLogo.center.x, y: 43))
+            frameAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+            frameAnimation.duration = 0.5
+            self.appLogo.layer.pop_addAnimation(frameAnimation, forKey: "frameAnimation")
+            
+            frameAnimation.completionBlock = { (POPAnimation, Bool) -> Void in
+                self.performSegueWithIdentifier("goToApp", sender: self)
+            }
+        }
+        
+        
+        
     }
-
+    
 }
 
